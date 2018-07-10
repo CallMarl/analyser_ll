853b8c5a49d2045138961cd383ddd9dd26b7c301
Paul PRIKAZSKY <pprikazs@student.42.fr>
2018-07-10 14:39:21 +0200

    Suppression de la fonction ft_access integration dans la libft

--- analyser_ll/srcs/export/ft_export_value.c
+++ analyser_ll/srcs/export/ft_export_value.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/28 14:22:46 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/07/06 11:08:08 by                  ###   ########.fr       */
+/*   Updated: 2018/07/10 14:38:09 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,96 +19,6 @@ static int			ft_export_lltab(int fd)
 {
 	(void)fd;
 	return (0);
-}
-
-static int			ft_useraccess(int right)
-{
-	int				ret;
-
-	ret = 0;
-	if (right >= 4)
-	{
-		ret |= S_IRUSR;
-		right -= 4;
-	}
-	if (right >= 2)
-	{
-		ret |= S_IWUSR;
-		right -= 2;
-	}
-	if (right == 1)
-		ret |= S_IXUSR;
-	return (ret);
-}
-
-static int			ft_otheraccess(int right)
-{
-	int				ret;
-
-	ret = 0;
-	if (right >= 4)
-	{
-		ret |= S_IROTH;
-		right -= 4;
-	}
-	if (right >= 2)
-	{
-		ret |= S_IWOTH;
-		right -= 2;
-	}
-	if (right == 1)
-		ret |= S_IXOTH;
-	return (ret);
-}
-
-static int			ft_groupaccess(int right)
-{
-	int				ret;
-
-	ret = 0;
-	if (right >= 4)
-	{
-		ret |= S_IRGRP;
-		right -= 4;
-	}
-	if (right >= 2)
-	{
-		ret |= S_IWGRP;
-		right -= 2;
-	}
-	if (right == 1)
-		ret |= S_IXGRP;
-	return (ret);
-}
-
-static int			ft_access(char *right)
-{
-	int				val;
-	int				cur;
-	int				ret;
-
-	ret = 0;
-	if (right == 0)
-		return (0);
-	if (!ft_strisnumber(right) || ft_strlen(right) != 3)
-		return (0);
-	val = ft_atoi(right);
-	if (val <= 0)
-		return (0);
-	cur = val % 10;
-	val /= 10;
-	ret |= ft_otheraccess(cur);
-	if (val == 0)
-		return (ret);
-	cur = val % 10;
-	val /= 10;
-	ret |= ft_groupaccess(cur);
-	if (val == 0)
-		return (ret);
-	cur = val % 10;
-	val /= 10;
-	ret |= ft_useraccess(cur);
-	return (ret);
 }
 
 extern int			ft_export_value(char *output_file)
