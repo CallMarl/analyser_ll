59fdf557cd5bc0da2efd3fb38ca0bdd06f11bda3
Paul PRIKAZSKY <pprikazs@student.42.fr>
2018-07-18 16:56:02 +0200

    Suppression des flag de debugging

--- libs/liball/liball_import/ft_import.c
+++ libs/liball/liball_import/ft_import.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 17:30:46 by                   #+#    #+#             */
-/*   Updated: 2018/07/18 15:25:57 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/07/18 16:50:03 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 ** -1 Erreur d'allocation
 ** -2 Erreur de lecture de fichier
 ** -3 Fichier trop grand
-** -4
 */
 
 extern int		ft_import(int fd, t_lltab *lltab, t_buff *llderi, t_buff *llterm)
@@ -29,17 +28,13 @@ extern int		ft_import(int fd, t_lltab *lltab, t_buff *llderi, t_buff *llterm)
 	ret = 0;
 	if ((ret = ft_read(fd, &gmr, FT_BUFF_SIZE)) < 0)
 		return (ret);
-	ft_putendl("ici");
 	cr = 0;
 	ret = ft_import_lltab(&gmr, lltab, &cr);
-	ft_putendl("ici");
 	if (ret < 0)
 		return (ret);
 	ret = ft_import_llderi(&gmr, llderi, &cr);
-	ft_putendl("ici");
 	if (ret < 0)
 		return (ret);
 	ret = ft_import_llterm(&gmr, llterm, &cr);
-	ft_putendl("ici");
 	return (ret);
 }
